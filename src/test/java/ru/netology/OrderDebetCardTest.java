827d4723119beba7a344f13ffbe9f2766a8c0fa1
YanaK <Yana.Koval15@yandex.ru>
2023-05-13 16:41:27 +0300

    positive Tests

--- src/test/java/ru/netology/OrderDebetCardTest.java
+++ src/test/java/ru/netology/OrderDebetCardTest.java
@@ -1,18 +1,20 @@
 package ru.netology;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class OrderDebetCardTest {
     @BeforeEach
     void setUpTests() {
         open("http://localhost:7777/");
     }
+
     @Test
-    void testPositive(){
+    void testPositive() {
         $("[data-test-id=name] input").setValue("Ковалевская Яна");
         $("[data-test-id=phone] input").setValue("+79896340085");
         $("[data-test-id=agreement]").click();
@@ -21,49 +23,53 @@ public class OrderDebetCardTest {
     }
 
     @Test
-    void testEnglishNaneNegative(){
+    void testEnglishNaneNegative() {
         $("[data-test-id=name] input").setValue("Kovalevskaya Yana");
         $("[data-test-id=phone] input").setValue("+79896340085");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $(withText("Имя и Фамилия указаные неверно.")).shouldBe(visible);
+
     }
+
     @Test
-    void testShortPhoneNegative(){
+    void testShortPhoneNegative() {
         $("[data-test-id=name] input").setValue("Ковалевская Яна");
         $("[data-test-id=phone] input").setValue("+79896340");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $(withText("Телефон указан неверно.")).shouldBe(visible);
     }
 
     @Test
-    void testNotAgreementNegative(){
+    void testNotAgreementNegative() {
         $("[data-test-id=name] input").setValue("Ковалевская Яна");
         $("[data-test-id=phone] input").setValue("+79896340085");
         $("[data-test-id=agreement]");
         $("button").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $(withText("Ваша заявка успешно отправлена!")).shouldNotBe(visible);
+
     }
 
     @Test
-    void testLongPhoneNegative(){
+    void testLongPhoneNegative() {
         $("[data-test-id=name] input").setValue("Ковалевская Яна");
         $("[data-test-id=phone] input").setValue("+798963400854585");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $(withText("Телефон указан неверно.")).shouldBe(visible);
+
     }
 
     @Test
-    void testPhone_8Negative(){
+    void testPhone_8Negative() {
         $("[data-test-id=name] input").setValue("Ковалевская Яна");
         $("[data-test-id=phone] input").setValue("89896340085");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $(withText("Телефон указан неверно.")).shouldBe(visible);
+        
     }
-    
 
 
 }
